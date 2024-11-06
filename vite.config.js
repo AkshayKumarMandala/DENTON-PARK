235c2949dc46a120815827bdbd52d8c2fad672dd
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-11-06 20:09:27 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import VitePluginVercel from 'vite-plugin-vercel';
 
-export default {
-  plugins: [VitePluginVercel()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePluginVercel()
+  ],
+  build: {
+    outDir: 'dist', // Specifies the output directory
+  },
+  server: {
+    port: 3000, // Port to run the development server on
+    open: true, // Automatically open the app in the browser
+  },
+});
