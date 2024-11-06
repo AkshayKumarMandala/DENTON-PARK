0a0ce49b1e95dcd21df1f18dd0e524937ba6a052
AkshayKumarMandala <akshaykumarmandala06@gmail.com>
2024-11-06 20:31:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,14 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000, // Increase chunk size limit to 1000 KB
     rollupOptions: {
+      external: [
+        // List of dependencies you don't want to include in the bundle
+        'axios', 'firebase', '@google/generative-ai'
+      ],
       output: {
         manualChunks(id) {
-          // Bundle all node_modules into a "vendor" chunk
           if (id.includes('node_modules')) {
-            return 'vendor';
+            return 'vendor'; // Bundle all node_modules into a "vendor" chunk
           }
         }
       }
